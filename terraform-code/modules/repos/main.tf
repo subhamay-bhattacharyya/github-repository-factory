685de1f77daec1ef4082274e32efdcd341db1d62
Subhamay Bhattacharyya <142895397+bsubhamay@users.noreply.github.com>
2025-07-27 01:44:53 +0000

    Add templates for README, CONTRIBUTING, CHANGELOG, and package.json; update visibility in JSON configuration

--- terraform-code/modules/repos/main.tf
+++ terraform-code/modules/repos/main.tf
@@ -15,3 +15,64 @@ resource "github_repository" "this" {
     repository = each.value.template
   }
 }
+
+resource "github_repository_file" "readme" {
+
+  for_each            = var.repos
+  repository          = github_repository.this[each.key].name
+  branch              = "main"
+  file                = "README.md"
+  overwrite_on_create = true
+  content = templatefile("${path.module}/../../templates/README.tftpl",
+    {
+      gh-org    = "subhamay-bhattacharyya"
+      heading   = each.value.workshop
+      repo-name = github_repository.this[each.key].name
+      iac       = each.value.iac
+      gist-id   = each.value["gist-id"]
+      email     = "subhamay.aws@gmail.com"
+    }
+  )
+}
+
+resource "github_repository_file" "contributing" {
+
+  for_each            = var.repos
+  repository          = github_repository.this[each.key].name
+  branch              = "main"
+  file                = "CONTRIBUTING.md"
+  overwrite_on_create = true
+  content = templatefile("${path.module}/../../templates/CONTRIBUTING.tftpl",
+    {
+      repo-name = github_repository.this[each.key].name
+    }
+  )
+}
+
+resource "github_repository_file" "package" {
+
+  for_each            = var.repos
+  repository          = github_repository.this[each.key].name
+  branch              = "main"
+  file                = "package.json"
+  overwrite_on_create = true
+  content = templatefile("${path.module}/../../templates/package.tftpl",
+    {
+      repo-name   = github_repository.this[each.key].name
+      gh-org      = "subhamay-bhattacharyya"
+      description = each.value.description
+
+    }
+  )
+}
+
+resource "github_repository_file" "changelog" {
+
+  for_each            = var.repos
+  repository          = github_repository.this[each.key].name
+  branch              = "main"
+  file                = "CHANGELOG.md"
+  overwrite_on_create = true
+  content             = file("${path.module}/../../templates/CHANGELOG.tftpl")
+}
+
